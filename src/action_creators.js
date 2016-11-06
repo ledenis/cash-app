df9a1de4ba278074bd91a7fb65621ef5a407066f
ledenis <ledenis.git@gmail.com>
2016-11-06 17:10:58 +0000

    App component: make 'count' be controlled input

---
+++ src/action_creators.js
@@ -0,0 +1,7 @@
+export function setItemCount(facial, count) {
+  return {
+    type: 'SET_ITEM_COUNT',
+    facial,
+    count
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
-import './App.css';
+import * as actionCreators from '../action_creators'
 
 class App extends Component {
+  handleCountChange(facial, count) {
+    this.props.setItemCount(facial, count)
+  }
+  
   render() {
+    // for each item, render facial and count
     return (
       <ul>
           {this.props.items.map(it => {
+            const facial = it.get('facial')
             return (
-              <li>
-                <h3>{it.get('facial')} €</h3>
-                <p>{it.get('count')}</p>
+              <li key={facial}>
+                <h3>{facial} €</h3>
+                <input value={it.get('count')}
+                  onChange={(e) =>
+                    this.handleCountChange(facial, e.target.value)}
+                  type="number" min="0" step="1"
+                />
               </li>
             )
           })}
@@ -26,6 +36,6 @@ function mapStateToProps(state) {
   }
 }
 
-const AppContainer = connect(mapStateToProps)(App)
+const AppContainer = connect(mapStateToProps, actionCreators)(App)
 
 export {App, AppContainer}
