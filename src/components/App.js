5f33dd35d3a9043ec30c5c03d620ca93e2c29c41
ledenis <ledenis.git@gmail.com>
2016-11-08 12:41:51 +0000

    App component: add total

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,17 @@ import {connect} from 'react-redux'
 import * as actionCreators from '../action_creators'
 
 class App extends Component {
+  // TODO: computed values from state should be done in another way
+  // maybe use Reselect?
+  calcTotal() {
+    //TODO
+    let total = 0
+    this.props.items.forEach(it => {
+      total += it.get('facial') * it.get('count')
+    })
+    return total
+  }
+  
   handleCountChange(facial, count) {
     this.props.setItemCount(facial, count)
   }
@@ -10,21 +21,24 @@ class App extends Component {
   render() {
     // for each item, render facial and count
     return (
-      <ul>
-          {this.props.items.map(it => {
-            const facial = it.get('facial')
-            return (
-              <li key={facial}>
-                <h3>{facial} €</h3>
-                <input value={it.get('count')}
-                  onChange={(e) =>
-                    this.handleCountChange(facial, e.target.value)}
-                  type="number" min="0" step="1"
-                />
-              </li>
-            )
-          })}
-      </ul>
+      <div>
+        <ul>
+            {this.props.items.map(it => {
+              const facial = it.get('facial')
+              return (
+                <li key={facial}>
+                  <h3>{facial} €</h3>
+                  <input value={it.get('count')}
+                    onChange={(e) =>
+                      this.handleCountChange(facial, e.target.value)}
+                    type="number" min="0" step="1"
+                  />
+                </li>
+              )
+            })}
+        </ul>
+        <p>Total : {this.calcTotal()} €</p>
+      </div>
     )
   }
 }
