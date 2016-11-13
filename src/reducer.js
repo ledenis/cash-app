d31d2c00470b0ccf0521128613e575b402e68fa9
ledenis <ledenis.git@gmail.com>
2016-11-13 20:59:25 +0000

    State: add type: 'BILL'|'COIN'

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,20 @@
 import {fromJS} from 'immutable'
 
-let facials = [500, 200, 100, 50, 20, 10, 5,
-  2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+const billFacials = [500, 200, 100, 50, 20, 10, 5]
+const coinFacials = [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
 let items  = []
-facials.forEach(facial => {
+billFacials.forEach(facial => {
   items.push({
     facial,
-    count: 0
+    count: 0,
+    type: 'BILL'
+  })
+})
+coinFacials.forEach(facial => {
+  items.push({
+    facial,
+    count: 0,
+    type: 'COIN'
   })
 })
 
