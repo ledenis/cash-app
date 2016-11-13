fdb63431cb373921ce31bb7a201c0c93004354e7
ledenis <ledenis.git@gmail.com>
2016-11-13 21:19:36 +0000

    App component: render bills and coins separately, plus separate totals

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   // TODO: computed values from state should be done in another way
   // maybe use Reselect?
   calcTotal() {
-    //TODO
     let total = 0
     this.props.items.forEach(it => {
       total += it.get('facial') * it.get('count')
@@ -14,39 +13,62 @@ class App extends Component {
     return total
   }
   
+  calcTotalByType(type) {
+    let total = 0
+    this.getItemsByType(type).forEach(it => {
+      total += it.get('facial') * it.get('count')
+    })
+    return total
+  }
+  
+  getItemsByType(type) {
+    return this.props.items.filter(it => it.get('type') === type)
+  }
+  
   handleCountChange(facial, count) {
     this.props.setItemCount(facial, count)
   }
   
+  renderItemsTable(type) {
+    // for each item, render facial, and count (input)
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Facial value</th>
+            <th>Count</th>
+          </tr>
+        </thead>
+          <tbody>
+            {this.getItemsByType(type).map(it => {
+              const facial = it.get('facial')
+              const countJsx = <input value={it.get('count')}
+                    onChange={(e) =>
+                      this.handleCountChange(facial, e.target.value)}
+                    type="number" min="0" step="1"
+                  />
+              return (
+                <tr key={facial}>
+                  <td>{facial} €</td>
+                  <td>{countJsx}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+      </table>
+    )
+  }
+  
   render() {
-    // for each item, render facial and count
     return (
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Facial value</th>
-              <th>Count</th>
-            </tr>
-          </thead>
-            <tbody>
-              {this.props.items.map(it => {
-                const facial = it.get('facial')
-                const countJsx = <input value={it.get('count')}
-                      onChange={(e) =>
-                        this.handleCountChange(facial, e.target.value)}
-                      type="number" min="0" step="1"
-                    />
-                return (
-                  <tr key={facial}>
-                    <td>{facial} €</td>
-                    <td>{countJsx}</td>
-                  </tr>
-                )
-              })}
-            </tbody>
-        </table>
-        <p>Total : {this.calcTotal().toFixed(2)} €</p>
+        <h4>Bills</h4>
+        {this.renderItemsTable('BILL')}
+        <p>Total : {this.calcTotalByType('BILL').toFixed(2)} €</p>
+        <h4>Coins</h4>
+        {this.renderItemsTable('COIN')}
+        <p>Total : {this.calcTotalByType('COIN').toFixed(2)} €</p>
+        <p><strong>TOTAL : {this.calcTotal().toFixed(2)} €</strong></p>
       </div>
     )
   }
