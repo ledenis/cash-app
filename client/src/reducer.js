b4601c8840847239d6bad8919062eadc67e63597
ledenis <ledenis.git@gmail.com>
2017-04-10 23:18:01 +0200

    Reducer: change state structure to user numeric keys

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,49 +1,45 @@
-import {fromJS} from 'immutable'
+import {Map} from 'immutable'
 
 const billFacials = [500, 200, 100, 50, 20, 10, 5]
 const coinFacials = [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
-let items  = []
+let items  = Map()
 billFacials.forEach(facial => {
-  items.push({
+  items = items.set(facial, Map({
     facial,
     count: 0,
     type: 'BILL'
-  })
+  }))
 })
 coinFacials.forEach(facial => {
-  items.push({
+  items = items.set(facial, Map({
     facial,
     count: 0,
     type: 'COIN'
-  })
+  }))
 })
 
 /*
 state is of the form: {
-  items: [
-    {
+  items: {
+    500: { // numeric key!
       facial: 500,
       count: 0,
       type: 'BILL'
     },
     ...
-  ]
+  }
 }
 */
 
-const INITIAL_STATE = fromJS({items})
+const INITIAL_STATE = Map({items})
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'SET_ITEM_COUNT':
     const facial = action.facial
     const count = action.count
-    
-    const ind = state.get('items').findIndex(it => {
-      return it.get('facial') === facial
-    })
-    
-    return state.setIn(['items', ind, 'count'], count)
+
+    return state.setIn(['items', facial, 'count'], count)
     
   default:
     return state
--- client/src/reducer.test.js
+++ client/src/reducer.test.js
@@ -1,27 +1,25 @@
 import {fromJS} from 'immutable'
+import {Map} from 'immutable'
 import reducer from './reducer'
 
 describe('reducer', () => {
   it('handles SET_ITEM_COUNT', () => {
     // given
-    const state = fromJS({
-      items: [{
-        facial: 500,
-        count: 2
-      }]
+    const state = Map({
+      items: Map([[
+        500, Map({
+          facial: 500,
+          count: 2
+        })
+      ]])
     })
     
     // when
     const newState = reducer(state, {type: 'SET_ITEM_COUNT', facial: 500, count: 54})
     
     // then
-    let ind = state.get('items')
-        .findIndex(i => i.facial === 500)
-    const count = state.getIn(['items', ind, 'count'])
-    
-    ind = newState.get('items')
-        .findIndex(i => i.facial === 500)
-    const newCount = newState.getIn(['items', ind, 'count'])
+    const count = state.getIn(['items', 500, 'count'])
+    const newCount = newState.getIn(['items', 500, 'count'])
     
     expect(count).toBe(2)
     expect(newCount).toBe(54)
