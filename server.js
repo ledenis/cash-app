8aaa533f6f251f8c516c0533b1e1ae1bd34be3c7
ledenis <ledenis.git@gmail.com>
2017-04-11 00:12:00 +0200

    Server: add GET /api/cash/:id

--- server.js
+++ server.js
@@ -4,9 +4,28 @@ const app = express()
 
 app.use(express.static('client/build'))
 
-app.get('/api/hello', (req, resp) => {
-  console.log('hello')
-  resp.send('hello')
+//TODO use a database...
+const cash = {
+  123: {
+    items: {
+      500: {
+        facial: 500,
+        count: 2
+      },
+      200: {
+        facial: 200,
+        count: 3
+      },
+    }
+  }
+}
+
+app.get('/api/cash/:id', (req, res) => {
+  const csh = cash[req.params.id]
+  if (csh === undefined) {
+    res.status(400).send('Not found')
+  }
+  res.json(csh)
 })
 
 const port = process.env.PORT || 3001
